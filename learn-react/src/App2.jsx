2ee0d7bfc47245dc5694e3c541261c2c278a72e7
krishnapal <krishnapalkp05@gmail.com>
2025-12-19 08:57:48 +0000

    Import-export

---
+++ learn-react/src/App2.jsx
@@ -0,0 +1,12 @@
+import Profile from "./Profile";
+import Profile2 from "./Profile";
+
+export default function Gallery() {
+    return (
+      <section>
+        <h1>Amazing scientists</h1>
+        <Profile />
+        <Profile2 />
+      </section>
+    );
+  }
